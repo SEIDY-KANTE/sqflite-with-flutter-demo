09a2b63d4f32d5d6c2b916c61be3d031e49dbf35
SEIDY-KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2023-06-10 18:12:35 +0300

    Implementing db_helper

--- lib/data/db_helper.dart
+++ lib/data/db_helper.dart
@@ -1,26 +1,61 @@
-
+import 'package:flutter/material.dart';
 import 'package:flutter_with_sqflite_demo/models/product.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DbHelper {
-
   late Database _db;
 
-  Future<Database> get db async{
+  Future<Database> get db async {
     _db ??= await initializeDb();
     return _db;
   }
-  Future<Database> initializeDb() async{
-    var databasesPath= await getDatabasesPath();
-    String path=join(databasesPath,'my_db.db');
-    var myDB=openDatabase(path,version: 1, onCreate: createDb);
+
+  Future<Database> initializeDb() async {
+    var databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'my_db.db');
+    var myDB = openDatabase(path, version: 1, onCreate: createDb);
     return myDB;
   }
-  void createDb(Database db, int version) async{
-    String sql='CREATE TABLE Products(id INTEGER PRIMARY KEY, name TEXT, description TEXT, unitPrice REAL)';
+
+  void createDb(Database db, int version) async {
+    String sql =
+        'CREATE TABLE Products(id INTEGER PRIMARY KEY, name TEXT, description TEXT, unitPrice REAL)';
     await db.execute(sql);
   }
 
 
+  Future<List<Product>> getProducts() async{
+    Database db=await this.db;
+    var result=await db.query("Products");
+    return List.generate(result.length, (index) => Product.fromObj(result[index]));
+  }
+
+  Future<int> insert(Product product) async{
+    Database db=await this.db;
+    
+    //with rawInsert
+    /*String query="INSERT INTO Products(id,name,description,unitPrice) VALUES(${product.id},${product.name},${product.description},${product.unitPrice})";
+    return db.rawInsert(query);*/
+    
+    //Another way
+    return db.insert("Products",product.toMap());
+  }
+  Future<int> update(Product product) async{
+    Database db=await this.db;
+    /*
+    return db.rawUpdate(
+    'UPDATE Products SET name = ?, description = ?, unitPrice=? WHERE id = ?',
+    ['updated name', 'description txt','unitPrice', 'id']);
+     */
+    var result=db.update("Products", product.toMap(),where: "id=?", whereArgs: [product.id]);
+    return result;
+  }
+
+  Future<int> delete(int id) async{
+    Database db= await this.db;
+    String sql="DELETE FROM Product where id=$id";
+    var result=await _db.rawDelete(sql);
+    return result;
+  }
 }
--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,10 +1,27 @@
-class Product{
- late int id;
-  String name;
-  String description;
-  double unitPrice;
+class Product {
+  late int id;
+  late String name;
+  late String description;
+  late double unitPrice;
 
-  Product.withID(this.id, this.name,this.description,this.unitPrice);
-  Product(this.name,this.description,this.unitPrice);
+  Product.withID(this.id, this.name, this.description, this.unitPrice);
+  Product(this.name, this.description, this.unitPrice);
 
+  Product.fromObj(dynamic obj) {
+    this.id = int.parse(obj["id"]);
+    this.name = obj["name"];
+    this.description = obj["description"];
+    this.unitPrice = double.parse(obj["unitPrice"]);
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{};
+    map["name"] = this.name;
+    map["description"] = this.description;
+    map["unitPrice"] = this.unitPrice;
+    if (id != null) {
+      map["id"] = this.id;
+    }
+    return map;
+  }
 }
